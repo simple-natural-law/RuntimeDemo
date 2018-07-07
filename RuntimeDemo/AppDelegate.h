beeb57d8d62327b15a6623fbbed57a2c10ba862c
zhangshijian <zsjcwy_xcode@163.com>
2018-07-07 10:46:38 +0800

    commit

---
+++ RuntimeDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RuntimeDemo
+//
+//  Created by 张诗健 on 2018/7/7.
+//  Copyright © 2018年 张诗健. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ RuntimeDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  RuntimeDemo
+//
+//  Created by 张诗健 on 2018/7/7.
+//  Copyright © 2018年 张诗健. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
