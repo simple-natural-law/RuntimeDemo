d9465d84cd7b1ff37dddd481dfcfe0ad0b9377f3
zhangshijian <zsjcwy_xcode@163.com>
2018-07-09 11:54:08 +0800

    commit

---
+++ RuntimeDemo/People.h
@@ -0,0 +1,15 @@
+//
+//  People.h
+//  RuntimeDemo
+//
+//  Created by 张诗健 on 2018/7/9.
+//  Copyright © 2018年 张诗健. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface People : NSObject
+
+- (void)eatApple;
+
+@end
---
+++ RuntimeDemo/People.m
@@ -0,0 +1,18 @@
+//
+//  People.m
+//  RuntimeDemo
+//
+//  Created by 张诗健 on 2018/7/9.
+//  Copyright © 2018年 张诗健. All rights reserved.
+//
+
+#import "People.h"
+
+@implementation People
+
+- (void)eatApple
+{
+    NSLog(@"people eat apple. ");
+}
+
+@end
